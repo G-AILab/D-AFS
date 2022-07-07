163679f487cf6f4748e9367f79bb087c22312e31
Gwen-JW <jiawennw@gmail.com>
2022-07-07 09:28:38 +0800

    initial commit

--- DQN_attention/cartpole/replay_buff/replay_buff.py
+++ DQN_attention/cartpole/replay_buff/replay_buff.py
@@ -1,11 +1,13 @@
-"""经验池，实现经验池存入与采样
+"""
+experience pool
 """
 import numpy as np
 import random
 
 class ReplayBuffer(object):
-    """经验池基础类
-    实现经验池数据存入与采样
+    """
+    store
+    sample
     """
     def __init__(self, size):
         self._storage = []
@@ -16,13 +18,13 @@ class ReplayBuffer(object):
         return len(self._storage)
     
     def add(self, obs_t, action, reward, obs_tp1, done):
-        """数据存入经验池
+        """
         Parameters
         ----------
-        obs_t：当前状态值
-        action：动作值
-        reward：奖励值
-        obs_tp1：下一时刻状态值
+        obs_t：current observation
+        action：action
+        reward：reward
+        obs_tp1：next observation
         """
         data = (obs_t, action, reward, obs_tp1, done)
         if self._next_idx >= len(self._storage):
@@ -44,15 +46,15 @@ class ReplayBuffer(object):
         return np.array(obses_t),np.array(actions),np.array(rewards),np.array(obses_tp1),np.array(dones)
 
     def sample(self, batch_size):
-        """数据采样
+        """
         Parameters:
         -----------
         batch_size：int
-            采样batch_size组数据
+            batch_size data
 
         return：
         **arg：np.ndarray 
-            随机采样获得得batch_size组数据
+            get batch_size data
             
         """
         idxes = [random.randint(0, len(self._storage)-1) for _ in range(batch_size)]
